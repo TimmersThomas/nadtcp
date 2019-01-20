8770f94047b4a3c736b13d52dc648c3b543008c5
Thomas Timmers <me@timmersthomas.be>
2019-01-20 15:56:32 +0100

    Updated readme + version

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # nadtcp
+
+# Forked version of [mindig.marton nadTCP](https://gitlab.com/mindig.marton/nadtcp)
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='nadtcp',
-      version='0.1.1',
+      version='0.1.2',
       description='Library for TCP/IP enabled NAD amplifiers. Currently only the C338 is supported.',
       url='https://gitlab.com/mindig.marton/nadtcp',
       download_url='https://gitlab.com/mindig.marton/nadtcp/repository/archive.zip?ref=0.1.1',
